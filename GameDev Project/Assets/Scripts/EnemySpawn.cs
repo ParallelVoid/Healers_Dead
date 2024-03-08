6adc124e84c983b3eca090d40bfd78f93f71c9ed
Saccad323 <allenchiang323@hotmail.com>
2024-03-07 20:13:47 -0800

    Import Scripts
    Enemy AI
    EnemySpawn
    EnemyStats
    LevelChange

---
+++ GameDev Project/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    [SerializeField]
+    private float enemyInterval = 3.5f;
+
+    private float enemyNumber;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(enemyInterval, enemyPrefab));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy){
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        enemyNumber = enemyNumber + 1;
+        if (enemyNumber < 10) {
+            StartCoroutine(spawnEnemy(interval, enemy));
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameDev Project/Assets/Scripts/Enemy_AI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AI : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject player;
+    public float speed;
+
+    public float distance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        if (distance < 4){
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+        
+
+    }
+
+}
