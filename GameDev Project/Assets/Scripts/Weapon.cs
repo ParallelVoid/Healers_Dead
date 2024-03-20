55fd883f251f6ec5c712f2cb9983ef5cbdfb3122
Saccad323 <allenchiang323@hotmail.com>
2024-03-19 17:31:21 -0700

    Add MainMenu & PauseMenu
    You can pause the game, resume after pause, go to temp main menu, or quit game

--- GameDev Project/Assets/Scripts/Weapon.cs
+++ GameDev Project/Assets/Scripts/Weapon.cs
@@ -15,18 +15,21 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
-
-        if (timeBtwShots <= 0) 
+        if(!PauseMenu.isPaused)
         {
-            if(Input.GetMouseButtonDown(0)) {
-            Instantiate(projectile, shotPoint.position, transform.rotation);
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+            if (timeBtwShots <= 0) 
+            {
+                if(Input.GetMouseButtonDown(0)) {
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                }
             }
-        }
-        else {
-            timeBtwShots -= Time.deltaTime;
+            else {
+                timeBtwShots -= Time.deltaTime;
+            }
         }
 
         
