71faced44d22eaddd5570cdc3b6c2ad9df5560d7
Saccad323 <allenchiang323@hotmail.com>
2024-03-23 18:07:37 -0700

    Fixed Lich Projectile
    - lich projectile can now disappear after a duration

--- GameDev Project/Assets/Scripts/LichProjectile.cs
+++ GameDev Project/Assets/Scripts/LichProjectile.cs
@@ -9,7 +9,7 @@ public class LichProjectile : MonoBehaviour
     public playerStats currentHealth;
     public int damage = 20;
     //private int health = 20;
-    //public float lifeTime;
+    public float lifeTime = 10f;
 
     public float distance;
     private Transform player;
@@ -18,8 +18,8 @@ public class LichProjectile : MonoBehaviour
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        //Invoke("DestroyProjectile", lifeTime);
-        //Destroy(gameObject, lifeTime);
+        Invoke("DestroyProjectile", lifeTime);
+        //Destroy(gameObject, lifeTime);()
     }
 
     // Update is called once per frame
@@ -31,6 +31,11 @@ public class LichProjectile : MonoBehaviour
         }
 
 
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("died");
     }
 
     void OnCollisionEnter2D (Collision2D collision) 
@@ -46,7 +51,7 @@ public class LichProjectile : MonoBehaviour
 
     void DestroyProjectile() 
     {
-        Destroy(gameObject);
+        Destroy(this.gameObject);
     
             
     }
