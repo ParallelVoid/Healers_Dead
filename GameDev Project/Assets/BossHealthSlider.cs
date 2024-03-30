27e9e7f243973ecac04a01b31ab37080123fb34b
Saccad323 <allenchiang323@hotmail.com>
2024-03-29 18:35:33 -0700

    Updated Cemetery Out of Bound Tiles

---
+++ GameDev Project/Assets/BossHealthSlider.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthSlider : MonoBehaviour
+{
+    public Slider slider;
+
+    // public void SetEnemyMaxHealth(int enemyhealth)
+    // {
+    //     slider.maxValue = enemyHealth;
+    //     slider.value = health;
+    // }
+
+    // public void SetEnemyHealth(int enemyhealth)
+    // {
+    //     slider.value = health;
+    // }
+}
