25aebd52661247c35dde96c1be3fde7e1a141e2a
Saccad323 <allenchiang323@hotmail.com>
2024-03-31 16:18:24 -0700

    Adding Audio Manager

--- GameDev Project/Assets/AudioManager.cs
+++ GameDev Project/Assets/AudioManager.cs
@@ -1,28 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
     
-    [SerializeField] AudioSource SFXSource;
-
-    public AudioClip UIButton;
-    public AudioClip playerAttack;
-    public AudioClip playerWalking;
-    public AudioClip playerHeal;
-
+    public Sound[] sounds;
     
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Start is called before the first frame update
+    // void Awake()
+    // {
+    //     foreach(Sound s in sounds)
+    //     {
+    //         s.source = gameObject.AddComponent<AudioSource>();
+    //         s.source.clip = s.clip;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //SFXSource.PlayOneShot(clip);
-    }
+    //         s.source.volume = s.volume;
+    //         s.source.pitch = s.pitch;
+    //     }
+    // }
+
+    // public void Play (string name)
+    // {
+    //     Sound s = Array.Find(sounds, sound => sound.name == name);
+    //     s.source.Play();
+    // }
 }
