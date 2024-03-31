7fdb14a247ca483f5609e5324af5da466a8e8a89
Saccad323 <allenchiang323@hotmail.com>
2024-03-31 15:40:14 -0700

    Added Audio Manager
    - add audio manager
    - adjusted light settings

---
+++ GameDev Project/Assets/AudioManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip UIButton;
+    public AudioClip playerAttack;
+    public AudioClip playerWalking;
+    public AudioClip playerHeal;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
